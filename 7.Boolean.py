3d4a958f03763c4a8f5369660ff0532f33c3cb2d
Md. Shakil Rana <159621858+MDshakilr@users.noreply.github.com>
2024-10-22 09:43:59 +0600

    Create 7.Boolean.py

---
+++ 7.Boolean.py
@@ -0,0 +1,19 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+
+a = 20
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+x=34
+y=12
+print(x>y)
+print(x==y)
